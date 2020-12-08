cda5f21a1f516bba884daca01beb5835bb021871
Gabriel Cabrejas <gabriel@cabrejas.net>
2020-12-08 19:08:46 +0100

    wip

--- data/migrations/20201208185627_fruits_table.js
+++ data/migrations/20201208185627_fruits_table.js
@@ -1,7 +1,7 @@
 
 exports.up = function(knex) {
   return knex.schema.createTable('fruits', table => {
-    // id: primary key, unique, not null, integers that auto-increment
+    // id: primary key, unique, not null (integers that auto-increment)
     table.increments();
     // name: unique, not null
     table.text('name', 128).unique().notNullable();
